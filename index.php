15e204320164b9d4cb8e960df9043c39a6ef9ed7
WarrioR1993 <warrior.men1993@gmail.com>
2016-10-20 20:16:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<DOCTYPE html>
+<html>
+
+<head>
+
+<title> Комп'ютерні системи та мережі </title>
+
+<meta name="keyword" content="div verstka">
+<meta name="description" content="My first div verstka">
+<meta charset="UTF-8">
+<link href="div-verstka-CSS.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+<div class="Body">
+
+<div class="Main-Frame">
+
+<div class="Header">
+<p>Комп'ютерні системи та мережі</p>
+</div>
+
+<div class="Left-menu">
+<a class="menu" href="tests\Start.html"> ***Посібники*** </a><br>
+<br>
+<a class="menu" href="labWork.html"> #Лабараторні# </a><br>
+<br>
+<a class="menu" href="tests\allTests.html"> ***Тести***</a><br>
+<br>
+</div>
+
+<div class="Work-Area">
+<p class="book"> ***МЕТОДИЧНІ ВКАЗІВКИ*** </p><br>
+<a class="PDF" href="tests\Resources\2014-15_LW_KSM_metoda.pdf"><img src="tests\img\pdf.png" alt="PDF" height="150"> </a><br>
+<br>
+<p class="book"> ***КУРСОВИЙ ПРОЕКТ*** </p><br>
+<a class="PDF" href="tests\Resources\KP.pdf"> <img src="tests\img\pdf.png" alt="PDF" height="150"></a><br>
+<br>
+
+</div>
+
+<div class="Footer">
+Copiright &copy; <a href="div-verstka.html">Головна сторінка</a>
+</div>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
